93c240235629e151949947afddf7996c1e0a3c6f
2100679b <2100679b@umich.mx>
2025-06-11 10:04:45 -0600

    Primera versión del proyecto

--- simulaciones/ruleta/script.js
+++ simulaciones/ruleta/script.js
@@ -1,17 +1,16 @@
 let contador = 1;
 
 function apostar(apuesta) {
-  // Simula la ruleta: 18 rojo, 18 negro, 1 verde
   const ruleta = [...Array(18).fill('rojo'), ...Array(18).fill('negro'), 'verde'];
   const resultado = ruleta[Math.floor(Math.random() * ruleta.length)];
 
-  const resultadoTexto = `La ruleta cayó en: <span class="${resultado}">${resultado.toUpperCase()}</span>. `;
-  const mensajeFinal = (resultado === apuesta) ? "¡Ganaste! 🎉" : "Perdiste. 😞";
+  // Mensaje con color
+  const resultadoHTML = `La ruleta cayó en: <span class="${resultado}">${resultado.toUpperCase()}</span>. `;
+  const mensaje = (resultado === apuesta) ? "¡Ganaste! 🎉" : "Perdiste. 😞";
 
-  // Mostrar resultado con colores
-  document.getElementById("resultado").innerHTML = resultadoTexto + mensajeFinal;
+  document.getElementById("resultado").innerHTML = resultadoHTML + mensaje;
 
-  // Agregar al historial
+  // Historial
   const historial = document.getElementById("historial");
   historial.innerHTML += `
     <p>🌀 <strong>Ronda ${contador++}:</strong> Apostaste a 
