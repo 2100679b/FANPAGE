adb83b40a9ce5a7b757e9a6a942cb2e2261583bb
2100679b <2100679b@umich.mx>
2025-06-11 09:28:06 -0600

    Primera versión del proyecto

---
+++ simulaciones/ruleta/script.js
@@ -0,0 +1,17 @@
+function apostar(apuesta) {
+  const ruleta = [...Array(18).fill('rojo'), ...Array(18).fill('negro'), 'verde'];
+  const resultado = ruleta[Math.floor(Math.random() * ruleta.length)];
+
+  let mensaje = `La ruleta cayó en: ${resultado.toUpperCase()}. `;
+
+  if (resultado === apuesta) {
+    mensaje += "¡Ganaste!";
+  } else {
+    mensaje += "Perdiste.";
+  }
+
+  document.getElementById("resultado").textContent = mensaje;
+
+  const historial = document.getElementById("historial");
+  historial.innerHTML += `<p>Resultado: ${resultado}</p>`;
+}
